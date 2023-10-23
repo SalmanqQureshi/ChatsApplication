dedef024d7863605deada3fb2007b3f2d51559b9
SalmanqQureshi <95340697+SalmanqQureshi@users.noreply.github.com>
2023-10-24 00:51:42 +0500

    some work achieve

--- app/src/main/java/com/example/simpleeasy/RegisterActivity.java
+++ app/src/main/java/com/example/simpleeasy/RegisterActivity.java
@@ -2,13 +2,33 @@ package com.example.simpleeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView textView;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        textView=findViewById(R.id.txt_loginactivity);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
